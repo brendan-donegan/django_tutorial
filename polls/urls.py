25a11e7504c381e413ec4700f018bab26f53ec6c
Brendan Donegan <brendan.j.donegan@gmail.com>
2013-06-25 18:46:08 +0100

    Re-arrange URL resolver, upgrade detail view to contain a form for voting, and implement the vote view.

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
+urlpatterns = patterns('polls.views',
     # Examples:
     # url(r'^$', 'django_tutorial.views.home', name='home'),
     # url(r'^django_tutorial/', include('django_tutorial.foo.urls')),
@@ -13,6 +9,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
-from polls.models import Poll
+from polls.models import Poll, Choice
 from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -9,10 +12,23 @@ def index(request):
 
 def detail(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': poll})
+    return render_to_response('polls/detail.html', {'poll': poll},
+                              context_instance=RequestContext(request))
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s" % poll_id)
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s" % poll_id)
+    import pdb; pdb.set_trace()
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render_to_response('polls/detail.html', {
+            'poll': poll,
+            'error_message': "You didn't select a choice."},
+            context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls.views.results', args=(poll.id,)))
